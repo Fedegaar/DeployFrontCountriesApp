4877756a87da0752bb1aeebdef8b7409051605a5
Fedegaar <fedegaar@gmail.com>
2022-07-07 15:24:14 -0300

    bug fixing

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -15,7 +15,7 @@ export const getCountries = () => {
     return async function (dispatch) {
         await axios.get("deploycountriesback.herokuapp.com/countries")
         .then((countries) => {
-            console.log("REDUX-ACTIONS, I'M THE DEPLOY", JSON.parse(countries))
+            console.log("REDUX-ACTIONS, I'M THE DEPLOY", JSON.stringify(countries))
             return dispatch ({
                 type: GET_COUNTRIES,
                 payload: countries.data
